cfc3cd1594924e7dae26e5639776233f3ad0d2ba
fengde <jun1st.feng@gmail.com>
2020-01-15 14:32:32 +0800

    fix issue

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,7 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-index-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/templates/blog-index.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/.cache/dev-404-page.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/templates/blog-post.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/404.js"))),
   "component---src-pages-confirm-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/confirm.js"))),
   "component---src-pages-react-components-unsubscribed-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/react-components-unsubscribed.js"))),
