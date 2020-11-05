afef3b914abeab318391f21bdfcf55badadf3e0b
fengde <jun1st.feng@gmail.com>
2020-11-05 10:25:07 +0800

    rebuild

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-blog-index-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/templates/blog-index.js"))),
-  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/templates/blog-post.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/pages/404.js"))),
-  "component---src-pages-confirm-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/pages/confirm.js"))),
-  "component---src-pages-react-components-unsubscribed-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/pages/react-components-unsubscribed.js"))),
-  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st.github.io/src/pages/thanks.js")))
+  "component---src-templates-blog-index-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/templates/blog-index.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/templates/blog-post.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/404.js"))),
+  "component---src-pages-confirm-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/confirm.js"))),
+  "component---src-pages-react-components-unsubscribed-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/react-components-unsubscribed.js"))),
+  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/fengqijun/Documents/workspace/jun1st-blog/src/pages/thanks.js")))
 }
 
